f0fbe8dcca8dcd8146d0bb33da3ad7988c591617
Rohit Dang <rohitdang@Rohits-MacBook-Pro.local>
2018-09-11 11:46:58 +1200

    Adding the header file

--- client/modules/App/components/Header/Header.js
+++ client/modules/App/components/Header/Header.js
@@ -1,46 +1,31 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router';
-import { FormattedMessage } from 'react-intl';
+import React, { Component } from "react";
 
-// Import Style
-import styles from './Header.css';
+class Navbar extends Component {
+  render() {
+    const {username, isLoggedIn, logout} = this.props;
 
-export function Header(props, context) {
-  const languageNodes = props.intl.enabledLanguages.map(
-    lang => <li key={lang} onClick={() => props.switchLanguage(lang)} className={lang === props.intl.locale ? styles.selected : ''}>{lang}</li>
-  );
+    const heading = isLoggedIn ? username : "Voting System";
 
-  return (
-    <div className={styles.header}>
-      <div className={styles['language-switcher']}>
-        <ul>
-          <li><FormattedMessage id="switchLanguage" /></li>
-          {languageNodes}
-        </ul>
+    let action;
+    if(isLoggedIn) {
+      action = <button className="btn btn-default" onClick={logout} style={{float: "right"}}>Logout</button>;
+    } 
+    else {
+      action = <div>
+                <button className="btn btn-default" style={{float: "right"}}>Login</button>
+                {/* <button className="btn btn-default mr-1" style={{float: "right"}}>Signup</button> */}
+              </div>;
+    }
+
+    return (
+      <div className="container">
+      <nav className="navbar navbar-light bg-light">
+        <span className="navbar-brand mb-0 h1">{heading}</span>
+        {action}
+      </nav>
       </div>
-      <div className={styles.content}>
-        <h1 className={styles['site-title']}>
-          <Link to="/" ><FormattedMessage id="siteTitle" /></Link>
-        </h1>
-        {
-          context.router.isActive('/', true)
-            ? <a className={styles['add-post-button']} href="#" onClick={props.toggleAddPost}><FormattedMessage id="addPost" /></a>
-            : null
-        }
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
-Header.contextTypes = {
-  router: PropTypes.object,
-};
-
-Header.propTypes = {
-  toggleAddPost: PropTypes.func.isRequired,
-  switchLanguage: PropTypes.func.isRequired,
-  intl: PropTypes.object.isRequired,
-};
-
-export default Header;
+export default Navbar;

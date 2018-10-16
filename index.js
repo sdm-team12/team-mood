16f06e53c4d831798d3b133d39acb92256ac4c12
Rohit Dang <rohitdang@Rohits-MacBook-Pro.local>
2018-10-16 16:46:07 +1300

    Fixing mongodb connection

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const passport = require('passport');
 const config = require('./config');
-const path = require('path');
 const port = process.env.PORT || 8000
 //const path = require('path');
 
@@ -14,7 +13,6 @@ var User = require('./server/models/user');
 // ORIGINAL
 require('./server/models').connect(config.dbUri);
 // NEW
-//require('./server/models').connect(process.env.MONGODB_URI);
 
 const app = express();
 
@@ -35,7 +33,6 @@ app.use(session({
 // Pass the passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
-
 // ISSUE - I think these are fucked up, they don't seem to be saving the user to a session. When I try to call it later it returns null 
 passport.serializeUser(function(user, done) {
   console.log("serializeUser called");
@@ -54,10 +51,8 @@ passport.deserializeUser(function(_id, done) {
 // Load Passport strategies
 const localSignupStrategy = require('./server/passport/local-signup');
 const localLoginStrategy = require('./server/passport/local-login');
-const FacebookStrategy = require('./server/passport/passport-facebook');
 passport.use('local-signup', localSignupStrategy);
 passport.use('local-login', localLoginStrategy);
-passport.use('facebook', FacebookStrategy);
 
 // Pass the authentication checker middleware
 const authCheckMiddleware = require('./server/middleware/auth-check');
--- server/middleware/auth-check.js
+++ server/middleware/auth-check.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken');
 const User = require('mongoose').model('User');
 const config = require('../../config');
+var ObjectId = require('mongodb').ObjectID;
+
 
 /**
  * The Auth Checker Middleware Function
@@ -9,23 +11,22 @@ module.exports = (req, res, next) => {
     if (!req.headers.authorization) {
         return res.status(401).end();
     }
-
+    
     // Get the last part from an authorization header string like "bearer token-value"
     const token = req.headers.authorization.split(' ')[1];
 
-    // Decode the token using a secret key-phrase
+    // Decode the token using a secret key-phrase    
     return jwt.verify(token, config.jwtSecret, (err, decoded) => {
         // The 401 code is for unauthorized status
         if (err) { return res.status(401).end(); }
 
+        
         const userId = decoded.sub;
-        console.log(userId);
 
         // Check if a user exists
-        return User.findById(userId, (userErr, user) => {
+        
+        return User.findOne({"_id" : ObjectId(userId)},  (userErr, user) => {
             if (userErr || !user) {
-                console.log(user);
-                console.log(userErr);
                 return res.status(401).end();
             }
 

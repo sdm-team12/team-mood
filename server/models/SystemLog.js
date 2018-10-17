f4598991789be3715f0aa77980926d96cb62322e
Rohit Dang <rohitdang@Rohits-MacBook-Pro.local>
2018-10-17 21:21:57 +1300

    Removing merge conflicts

--- server/models/SystemLog.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-const Schema = mongoose.Schema;
-
-const SystemLogSchema = new Schema({
-  date: { type: 'date', default: Date.now, required: false},
-  description: { type: 'string', required: true },
-});
-
-export default mongoose.model('SystemLog', SystemLogSchema);
